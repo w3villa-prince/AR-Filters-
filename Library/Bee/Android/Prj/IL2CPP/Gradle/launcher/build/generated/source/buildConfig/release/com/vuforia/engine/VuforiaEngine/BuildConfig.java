f6f8507225bd96118e754fea0820b4cfd6c2cd15
w3villa-prince <prince.yadav@w3villa.com>
2024-01-28 11:02:21 +0530

    Firstcommit

---
+++ Library/Bee/Android/Prj/IL2CPP/Gradle/launcher/build/generated/source/buildConfig/release/com/vuforia/engine/VuforiaEngine/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vuforia.engine.VuforiaEngine;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.vuforia.engine.VuforiaEngine";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 10163;
+  public static final String VERSION_NAME = "10.16.3";
+}
---
+++ Library/Bee/Android/Prj/IL2CPP/Gradle/launcher/build/intermediates/metadata_library_dependencies_report/release/dependencies.pb
@@ -0,0 +1,16 @@
+
+C
+
+com.google.arcore*1.32.0"
+ MÞû¤$Œ×,9”æDÄÄ”ýô64zÐËPZ’²
+N
+(
+androidx.annotation
+annotation*1.3.0"
+ —ÜE¯ïã¡ä!ÚB¸¶éù‘G|Eüax >:^Šî…S
+5
+VuforiaEngine"
+ Á¸±ie_ƒ(»M0Ž&§´~À5žÊ7yô÷«-Ë
+$"
+ Î<É±Äòlë[wB)„ËÈÔé"É>Žð*·\BìD	
+base
